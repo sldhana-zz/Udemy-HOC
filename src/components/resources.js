5f407a1989939a33df31bb4fd916e84f119bb965
Dhana Shunmugasundram <dhanas@surveymonkey.com>
2016-08-07 22:08:56 -0700

    hoc

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+const Resources = () => {
+  return (
+    <div>
+      <ul>
+        <li>One</li>
+        <li>Two</li>
+      </ul>
+    </div>
+  )
+};
+
+export default Resources;
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import AuthenticationReducer from './authentication';
+
+const rootReducer = combineReducers({
+  authenticated: AuthenticationReducer
+});
+
+export default rootReducer;
